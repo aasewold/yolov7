5e6784f9af70c72ee513450520b6d2b7d6115a7b
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-11-23 14:25:26 +0100

    train.py: remove copy-paste augmentation

--- train.py
+++ train.py
@@ -676,9 +676,7 @@ if __name__ == '__main__':
                 'flipud': (1, 0.0, 1.0),  # image flip up-down (probability)
                 'fliplr': (0, 0.0, 1.0),  # image flip left-right (probability)
                 'mosaic': (1, 0.0, 1.0),  # image mixup (probability)
-                'mixup': (1, 0.0, 1.0),   # image mixup (probability)
-                'copy_paste': (1, 0.0, 1.0),  # segment copy-paste (probability)
-                'paste_in': (1, 0.0, 1.0)}    # segment copy-paste (probability)
+                'mixup': (1, 0.0, 1.0)}  # image mixup (probability)
         
         with open(opt.hyp, errors='ignore') as f:
             hyp = yaml.safe_load(f)  # load hyps dict
